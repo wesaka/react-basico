f6163ab30049f957d2cc6b0c3dace4a183380621
wesaka <kalatai.alberti@gmail.com>
2019-12-24 12:00:50 -0300

    Updates

---
+++ precache-manifest.0f5f502ce91a06279c5d1f477e015085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beac83d6a2f8ae0fa438eb66e99bd460",
+    "url": "/react-basico/index.html"
+  },
+  {
+    "revision": "1d4b8763e8a693aff127",
+    "url": "/react-basico/static/css/main.c2076e88.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/react-basico/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-basico/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d4b8763e8a693aff127",
+    "url": "/react-basico/static/js/main.b19deb13.chunk.js"
+  },
+  {
+    "revision": "ca0b120cca586ca457d7",
+    "url": "/react-basico/static/js/runtime-main.5cc4d1a3.js"
+  }
+]);
